40485a532242ac853145572b29f14aae22269ab6
Zieho Choi <jeff426@naver.com>
2021-07-16 14:51:34 +0900

    cam on

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Webcam from "react-webcam";
+import {useRef} from "react";
+
+function App() {
+  const webRef = useRef(null);
+  let img = "httpsL;';'";
+  const showImage = () => {
+    console.log(webRef.current.getScreenshot());
+  }
+  return (
+    <div className="App">
+      React Webcam
+      <Webcam ref = {webRef} />
+      <button Onclick = {() => { 
+      showImage();
+    }}> Console </button>
+
+    <br /> 
+    <img src = {img} alt="image"/>
+    </div>
+  );
+}
+
+export default App;
